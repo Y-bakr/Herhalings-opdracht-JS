8c223927b018d8cfe0061c09ee120984d5db4992
Y-bakr <302786836@student.rocmondriaan.nl>
2023-11-10 12:51:37 +0100

    Herhalings Opdracht js

---
+++ main.js
@@ -0,0 +1,88 @@
+console.log('main.js is running')
+
+//opdracht 1
+
+
+
+
+//opdracht 2
+
+//opdracht 3
+
+//opdracht 4
+
+
+
+
+//opdracht 5
+
+let euros = prompt("vul een bedrag met euro's");
+
+        /// omzetten van een string naar getal
+
+euros = Number (euros);
+
+const crypto =  prompt ("Kies een crypto (BTC, ETH, etc ...")
+
+if(crypto === 'BTC'){ 
+        /// dan doen we hier bitcoin berekening
+let result = (1/35050) *euros;
+console.log("BITCOINS: ", result);
+
+} else if(crypto === 'ETH') {
+
+        /// dan doen we hier ETH berekening
+
+ let result = (1/1887) *euros;
+ console.log("ETH: ", result);
+
+} else if(crypto === 'XRP') {
+
+        /// dan doen we hier XRP berekening
+   
+        let result = (1/0.7) *euros;
+console.log("XRP: ", result);
+
+} else if(crypto === 'DOG') {
+
+        /// dan doen we hier DOG berekening
+   
+        let result = (1/0.07) *euros;
+console.log("DOG: ", result);
+
+} else if(crypto === 'SOL') {
+
+     /// dan doen we hier SOL berekening
+   
+     let result = (1/40.26) *euros;
+console.log("SOL: ", result);
+
+}
+   
+   
+
+//opdracht 6
+
+//opdracht 7
+
+//opdracht 8
+
+//opdracht 9
+
+//opdracht 10
+
+//opdracht 11
+
+//opdracht 12
+
+//opdracht 13
+
+//opdracht 14
+
+//opdracht 15
+
+//opdracht 16
+
+
+
+
